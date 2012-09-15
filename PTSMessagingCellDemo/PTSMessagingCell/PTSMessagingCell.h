954aa5327e4cf5fecf6fa8e048f14bda457a4e36
Ralph Gasser <ralph.gasser@pontius-software.ch>
2012-09-15 19:30:38 +0200

    Some cleanup in the demo project.

--- PTSMessagingCellDemo/PTSMessagingCell/PTSMessagingCell.h
+++ PTSMessagingCellDemo/PTSMessagingCell/PTSMessagingCell.h
@@ -1,14 +1,26 @@
-//
-//  PTSMessagingCell.h
-//  Purplemoon
-//
-//  *View*
-//
+/*
+ PTSMessagingCell.h
+ 
+Copyright (C) 2012 pontius software GmbH
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #import <UIKit/UIKit.h>
 
 /** \class PTSMessagingCell
-    \brief This class extends a UITableViewCell with a style similar to that of the SMS-App (iOS). It displays a TextMessage of any size (only limited by the capabilities of UIView), a timestamp (if given) and an Avatar-Image (if given). 
+    \brief This class extends a UITableViewCell with a style similar to that of the SMS-App (iOS). It displays a text-message of any size (only limited by the capabilities of UIView), a timestamp (if given) and an avatar-Image (if given). 
  
     The cell will properly respond to orientation-changes and can be displayed on iPhones and iPads. The usage of this class is very simple: Initialize it, using the initMessagingCellWithReuseIdentifier:(NSString*)reuseIdentifier-Method and afterwards set its properties, as you would with a commom UITabelViewCell.
  
@@ -19,7 +31,6 @@
     @version 1.5
     @copyright Copyright 2012, pontius software GmbH
  */
-
 
 @interface PTSMessagingCell : UITableViewCell {
     /*Subview of the MessaginCell, containing the Avatar-Image (if specified). It can be set in the cellForRowAtIndexPath:-Method.*/
